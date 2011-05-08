2fc7e770693a3952f88d5e17796b7ec6a059e816
Wil Alvarez <wil_alejandro@yahoo.com>
2011-05-08 20:09:31 -0300

    Added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ ActionController::Routing::Routes.draw do |map|
   # See how all your routes lay out with "rake routes"
   map.session_error 'session/error', :controller => 'session', :action => 'error'
   map.resources :session
+  map.resources :users
   
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
