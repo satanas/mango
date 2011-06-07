4f176c56a9949282da8693e319a4506d19a9787a
Wil Alvarez <wil_alejandro@yahoo.com>
2011-06-07 00:11:48 -0300

    Implemented validation adding ingredients to recipes

--- app/controllers/ingredients_recipes_controller.rb
+++ app/controllers/ingredients_recipes_controller.rb
@@ -5,19 +5,24 @@ class IngredientsRecipesController < ApplicationController
 
   def create
     puts "create: #{params.inspect}"
+    @error = nil
     @recipe = Recipe.find(params[:recipe_id], :include=>'ingredient_recipe')
     ingredient = Ingredient.find_by_code(params[:ingredient_recipe][:code])
-    puts ingredient.inspect
-    ing_recipe = IngredientRecipe.new
-    ing_recipe.ingredient = ingredient
-    ing_recipe.recipe = @recipe
-    ing_recipe.priority = params[:ingredient_recipe][:priority]
-    ing_recipe.amount = params[:ingredient_recipe][:amount]
-    ing_recipe.percentage = params[:ingredient_recipe][:percentage]
-    ing_recipe.save
-    puts ing_recipe.inspect
-    index()
-    puts @recipe.inspect
+    
+    ingredient_recipe = IngredientRecipe.new
+    ingredient_recipe.ingredient = ingredient
+    ingredient_recipe.recipe = @recipe
+    ingredient_recipe.priority = params[:ingredient_recipe][:priority]
+    ingredient_recipe.amount = params[:ingredient_recipe][:amount]
+    ingredient_recipe.percentage = params[:ingredient_recipe][:percentage]
+
+    if ingredient_recipe.valid?
+      ingredient_recipe.save
+      index()
+    else
+      @error = "No se pudo guardar el ingrediente"
+    end
+
     respond_to do |format|
       format.js { render :layout=>false }
     end
@@ -25,9 +30,16 @@ class IngredientsRecipesController < ApplicationController
 
   def destroy
     puts "destroy: #{params.inspect}"
-    ingredient_recipe = IngredientRecipe.find(params[:id])
-    ingredient_recipe.destroy
-    index()
+    @error = nil
+    begin
+      ingredient_recipe = IngredientRecipe.find(params[:id])
+      ingredient_recipe.destroy
+      index()
+      raise Exception
+    rescue Exception => ex
+      puts ex.inspect
+      @error = "No se pudo borrar el ingrediente de la receta"
+    end
     respond_to do |format|
       format.js { render :layout=>false }
     end
--- app/models/ingredient_recipe.rb
+++ app/models/ingredient_recipe.rb
@@ -5,7 +5,7 @@ class IngredientRecipe < ActiveRecord::Base
   validates_presence_of :amount, :priority, :percentage
   validates_numericality_of :amount, :priority, :percentage
   
-  before_save :validate_existence
+  before_validation :validate_existence
   
   def validate_existence
     begin
@@ -18,6 +18,10 @@ class IngredientRecipe < ActiveRecord::Base
       recipe = Recipe.find(self.recipe_id)
     rescue Exception => ex
       errors.add(:recipe_id, "must exist")
+    end
+
+    if IngredientRecipe.find(:first, :conditions => ["ingredient_id = ? and recipe_id = ?", self.ingredient_id, self.recipe_id])
+      errors.add_to_base("ingredient already exist")
     end
     return (errors.size > 0) ? false : true
   end
