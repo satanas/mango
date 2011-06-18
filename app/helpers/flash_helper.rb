c01430256f1046b80bc52760a48bf3cd997ecb44
Wil Alvarez <wil_alejandro@yahoo.com>
2011-06-18 16:17:13 -0300

    Fixed flash notice styles

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -3,7 +3,7 @@ module FlashHelper
   def render_flash_notice
     return content_tag('div', '', :id=>'notice') unless flash[:notice]
     
-    flash[:type] = flash[:type] || 'info'
+    flash[:type] = flash[:type] || 'error'
     html = content_tag('div', flash[:notice], :id=>'notice', :class=>"notice #{flash[:type]}")
     html += javascript_tag('setTimeout("new Effect.Opacity(\'notice\', {from:1, to:0});",5000);
       setTimeout("$(\'notice\').update('');",5800);')
