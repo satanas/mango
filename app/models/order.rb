fddad85fc206b5a3766032f2230721f43a9afe0c
Wil Alvarez <wil_alejandro@yahoo.com>
2011-08-31 01:04:39 -0300

    Fixed validation of batches in orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,8 @@ class Order < ActiveRecord::Base
   belongs_to :product
   has_many :batch
   
-  validates_presence_of :code, :prog_batchs, :recipe_id, :user_id
+  validates_presence_of :recipe_id, :user_id
   validates_uniqueness_of :code
+  validates_numericality_of :prog_batchs, :real_batchs, :only_integer => true, :greater_than_or_equal_to => 0
   validates_associated :recipe, :client, :user
 end
