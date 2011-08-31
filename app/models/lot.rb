fef1e3cec16aa84df7ec201d1591db26ecddfdb8
Wil Alvarez <wil_alejandro@yahoo.com>
2011-08-30 21:17:35 -0300

    Added ingredient to lot list in hopper new form

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -6,4 +6,12 @@ class Lot < ActiveRecord::Base
   validates_uniqueness_of :code
   validates_presence_of :location, :date, :ingredient_id
   validates_length_of :code, :within => 3..20
+
+  def self.find_all
+    find :all, :include => ['ingredient'], :order => 'code ASC'
+  end
+
+  def to_collection_select
+   "#{self.code} - #{self.ingredient.name}"
+  end
 end
