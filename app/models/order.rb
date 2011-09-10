193fbf94f34d548806f2e5e12cb4d517b51f207d
Wil Alvarez <wil_alejandro@yahoo.com>
2011-09-10 09:58:21 -0300

    Fixed issue with real batches in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,13 @@ class Order < ActiveRecord::Base
   
   validates_presence_of :recipe_id, :user_id
   validates_uniqueness_of :code
-  validates_numericality_of :prog_batchs, :real_batchs, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :prog_batchs, :real_batchs, :only_integer => 0, :greater_than_or_equal_to => 0
   validates_associated :recipe, :client, :user
+
+  before_validation :validates_real_batchs
+
+  def validates_real_batchs
+    self.real_batchs = 0 if self.real_batchs.nil?
+    return true
+  end
 end
