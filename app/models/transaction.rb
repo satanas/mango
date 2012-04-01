3cea532224f9e0265be0eb200d79b3fb3967ff03
Eleazar Meza <meza.eleazar@gmail.com>
2012-04-01 18:55:28 -0430

    Fixed increase_stock & decrease_stock methods to properly handle the transaction_type sign

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,11 +7,32 @@ class Transaction < ActiveRecord::Base
   validates_presence_of :amount, :date, :transaction_type_id, :warehouse_id, :document_number
 
   before_save :create_code
-  after_save :increase_stock
-  after_destroy :decrease_stock
+  after_save :do_stock_update
+  after_destroy :undo_stock_update
 
   private
 
+  def get_sign
+    transaction_type = TransactionType.find(self.transaction_type_id)
+    return transaction_type.sign
+  end
+  
+  def do_stock_update
+    if get_sign == '+'
+      increase_stock
+    else
+      decrease_stock
+    end
+  end
+  
+  def undo_stock_update
+    if get_sign == '-'
+      increase_stock
+    else
+      decrease_stock
+    end
+  end
+  
   def create_code
     last = Transaction.last
     if last.nil?
