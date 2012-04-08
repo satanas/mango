e88559dbd33fabf4a279066cf1914842183bd505
Eleazar Meza <meza.eleazar@gmail.com>
2012-04-08 08:55:23 -0430

    More manly sample database.yml file, also, someone touched schema.rb, I swear it wasn't me, fo sho

--- db/schema.rb
+++ db/schema.rb
@@ -189,13 +189,6 @@ ActiveRecord::Schema.define(:version => 20120405224311) do
     t.datetime "updated_at"
   end
 
-  create_table "roles_users", :force => true do |t|
-    t.integer  "role_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "schedules", :force => true do |t|
     t.string   "name"
     t.time     "start_hour"
