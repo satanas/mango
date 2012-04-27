11d612428166fab7b32b80fb1d21e96b95905bbe
satanas <wil.alejandro@gmail.com>
2012-04-27 01:20:51 -0300

    Fixed minor issues with warehouse module

--- app/models/product_lot.rb
+++ app/models/product_lot.rb
@@ -5,4 +5,8 @@ class ProductLot < ActiveRecord::Base
   validates_uniqueness_of :code
   validates_presence_of :date, :product_id
   validates_length_of :code, :within => 3..20
+
+  def to_collection_select
+   "#{self.product.name} (L: #{self.code})"
+  end
 end
